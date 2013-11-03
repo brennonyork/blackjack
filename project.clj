eaf65719e94131a2f5765a0ea6bb949eecab220f
Brennon York <brennon.york@gmail.com>
2013-11-03 14:53:36 -0500

    Updated project.clj to locally add the marginalia documentation generation and created as alias for document generation with 'lein docs'

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject blackjack "2.0.0"
   :description "Simple command-line blackjack game"
   :dependencies [[org.clojure/clojure "1.5.1"]]
+  :plugins [[lein-marginalia "0.7.1"]]
   :aot :all
   :main blackjack.core
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :aliases {"docs" "marg"})
